b91f49174ffd6e5939c5cd8333dd44a98ce0b48b
SuganyaArvind <suganyal.2012@gmail.com>
2022-04-05 11:04:24 +0530

    arvind

--- src/hotel/book/AdactinHotel.java
+++ src/hotel/book/AdactinHotel.java
@@ -25,7 +25,7 @@ public static void main(String[] args) throws InterruptedException {
 	WebElement ddn = driver.findElement(By.id("location"));
 	Select s = new Select(ddn);
 	s.selectByIndex(7);
-System.out.println("git");	
+System.out.println("bot");	
 System.out.println("branch created");
 	WebElement hotelD1 = driver.findElement(By.id("hotels"));
 	Select s1 = new Select(hotelD1);
